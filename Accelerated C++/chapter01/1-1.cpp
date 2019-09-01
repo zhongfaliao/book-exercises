ba6c752856289eea0232451a54f87f574f837d98
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-01 18:10:44 -0400

    Update 1-1.cpp

--- Accelerated C++/chapter01/1-1.cpp
+++ Accelerated C++/chapter01/1-1.cpp
@@ -3,7 +3,7 @@ using std::string;
 
 int main() {
 	const string hello = "Hello"; 
-  // when a constant variable is defined, it must be initialized simultaneous
+  	// when a constant variable is defined, it must be initialized simultaneous
 	const string message = hello + ", world" + "!";
 	return 0;
 }
