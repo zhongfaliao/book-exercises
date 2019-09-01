f7e98520bac8b58113cc6883024cdaa2037df2c8
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-01 17:13:30 -0400

    Create 0-7.cpp

---
+++ Accelerated C++/chapter00/0-7.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main() {
+      /* will this work /* or not */ when comments are interlinked? */
+			std::cout << "hello" << std::endl;
+      return 0;
+}
+// Answer is no.
