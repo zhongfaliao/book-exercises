73e09de06201603fc92719a637a84d1654b6bb45
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-01 18:12:30 -0400

    Create 1-2.cpp

---
+++ Accelerated C++/chapter01/1-2.cpp
@@ -0,0 +1,12 @@
+#include <string>
+using std::string;
+
+int main() {
+	const string exclam = "!"; 
+  	// when a constant variable is defined, it must be initialized simultaneous
+    
+	const string message = "Hello" + ", world" + exclam;
+  // the above is wrong
+  // cannot add two string literals: "Hello" and ", world"
+	return 0;
+}
