5ec5787b06221fc430c2d277308da2db6d25b6f5
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-01 17:09:20 -0400

    Create 0-6.cpp

---
+++ Accelerated C++/chapter00/0-6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main() {
+	{
+		{
+			std::cout << "hello" << std::endl;
+		}
+	}
+}
+// no mater how many braces, the program works
+// also note, there is no RETURN statements BUT still works
