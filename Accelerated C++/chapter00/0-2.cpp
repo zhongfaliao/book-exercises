e621e38f57c1169ca96288bf5134aafb1d296408
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-01 16:56:57 -0400

    Update 0-2.cpp

--- Accelerated C++/chapter00/0-2.cpp
+++ Accelerated C++/chapter00/0-2.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 
 int main() {
-  std::cout << "This (\") is a quote, and this (\\) is a backslash." 
-	    << std::endl;  
-
+  std::cout << "This (\") is a quote, and this (\\) is a backslash." << std::endl;  
   return 0;
 }
