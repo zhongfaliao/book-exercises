dc7bd123e95072652a9d3177c1e73c117c8fc079
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-01 21:32:17 -0400

    Update avg.cc

--- Accelerated C++/chapter03/avg.cc
+++ Accelerated C++/chapter03/avg.cc
@@ -1,50 +1,52 @@
-#include <iomanip>
-#ifndef __GNUC__
-#include <ios>
-#endif
-#include <iostream>
-#include <string>
+#include<iomanip>
+#include<iostream>
+#include<ios>
+#include<string>
 
-using std::cin;                  using std::setprecision;
-using std::cout;                 using std::string;
-using std::endl;                 using std::streamsize;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::setprecision;
+using std::streamsize;
 
-int main() {
-  // ask for and read the student's name
-  cout << "Please enter your first name: ";
-  string name;
-  cin >> name;
-  cout << "Hello, " << name << "!" << endl;
+int main()
+{
+	//ask for and read the studnet's name
+	cout << "Please enter your first name: ";
+	string name;
+	cin >> name;
+	cout << "Hello, " << name << "!" << endl;
 
-  // ask for and read the midterm and final grades
-  cout << "Please enter your midterm and final exam grades: ";
-  double midterm, final;
-  cin >> midterm >> final;
+	//ask for and read the midterm and final grades
+	cout << "Please enter your midterm and final exam grades: ";
+	double midterm, final;
+	cin >> midterm >> final;
 
-  // ask for the homework grades
-  cout << "Enter all your homework grades, "
-    "followed by end-of-file: ";
+	//ask for the homework grades
+	cout << "Enter all your homework grades, "
+		"followed by end-of-file: ";
 
-  // the number and sum of grades read so far
-  int count = 0;
-  double sum = 0;
+	//the number and sum of grades read so far
+	int count = 0;
+	double sum = 0;
 
-  // a variable into which to read
-  double x;
+	//a variable into which to read
+	double x;
+	//invariant:
+	//we have read count grades so far, and
+	//sum is the sum of the first count grades
 
-  // invariant:
-  //     we have read `count' grades so far, and
-  //     `sum' is the sum of the first `count' grades
-  while (cin >> x) {
-    ++count;
-    sum += x;
-  }
+	while (cin >> x) {
+		++count;
+		sum += x;
 
-  // write the result
-  streamsize prec = cout.precision();
-  cout << "Your final grade is " << setprecision(3)
-       << 0.2 * midterm + 0.4 * final + 0.4 * sum / count
-       << setprecision(prec) << endl;
+	}
 
-  return 0;
+	//write the result
+	streamsize prec = cout.precision();
+	cout << "Your final grade is " << setprecision(3)
+		<< 0.2*midterm + 0.4*final + 0.4*sum / count
+		<< setprecision(prec) << endl;
+		
 }
