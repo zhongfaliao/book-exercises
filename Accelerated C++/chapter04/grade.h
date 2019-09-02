8976fcb65e4b694258c9229a99730fe75b33723f
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-02 16:59:39 -0400

    Update grade.h

--- Accelerated C++/chapter04/grade.h
+++ Accelerated C++/chapter04/grade.h
@@ -1,13 +1,15 @@
 #ifndef GUARD_grade_h
 #define GUARD_grade_h
 
-// `grade.h'
+// 'grade.h'
+
 #include <vector>
 #include "Student_info.h"
+
+// overloading the function with different settings of arguments
 
 double grade(double, double, double);
 double grade(double, double, const std::vector<double>&);
 double grade(const Student_info&);
 
 #endif
-
