63e288ae3a493bf4799c3c489652623c65a05115
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-02 01:09:57 -0400

    Update 3-2.cpp

--- Accelerated C++/chapter03/3-2.cpp
+++ Accelerated C++/chapter03/3-2.cpp
@@ -1,62 +1,36 @@
-#include <algorithm>
 #include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
 
-#ifdef _MSC_VER
-  typedef std::vector<int>::size_type vec_sz;
-#else
-  typedef vector<int>::size_type vec_sz;
-#endif
-
-double median(const vector<int>& integers, vec_sz begin, vec_sz end) {
-  vec_sz size = begin + end;
-  vec_sz mid = size / 2;
-  return size % 2 == 0 ?
-    (integers[mid] + integers[mid - 1]) / 2.0 :
-    integers[mid];
-}
-
 int main() {
-  vector<int> integers;
-  int x;
+	typedef vector<string>::size_type vec_sz;
 
-  cout << "Integers: ";
+	vector<string> words;
+	vector<int> counts;
 
-  while (cin >> x)
-    integers.push_back(x);
+	cout << "Words: ";
+	string s;
 
-  if (integers.size() == 0) {
-    cout << endl << "No integers!" << endl;
-    return 1;
-  }
+	while (cin >> s) {
+		bool found = false;
 
-  sort(integers.begin(), integers.end());
+		for (vec_sz i = 0; i < words.size(); ++i) {
+			if (s == words[i]) {
+				++counts[i];
+				found = true;
+			}
+		}
 
-  vec_sz mid = integers.size() / 2;
-  vec_sz lower_half_end;
-  vec_sz upper_half_begin;
+		if (!found) {
+			words.push_back(s);
+			counts.push_back(1);
+		}
+	}
 
-  // compute quartiles using Method 1 from: https://en.wikipedia.org/wiki/Quartile
-  if (integers.size() % 2 != 0) {
-    // odd number of data points, exclude median from either half
-    lower_half_end = mid;
-    upper_half_begin = mid + 1;
-  } else {
-    // even number of data points, split data set exactly in half
-    lower_half_end = mid;
-    upper_half_begin = mid;
-  }
+	for (vec_sz i = 0; i < words.size(); ++i)
+		cout << words[i] << " appeared " << counts[i] << " times" << endl;
 
-  cout << "Q1" << endl;
-  cout << median(integers, 0, lower_half_end) << endl;
-
-  cout << "Q2" << endl;
-  cout << median(integers, 0, integers.size()) << endl;
-
-  cout << "Q3" << endl;
-  cout << median(integers, upper_half_begin, integers.size()) << endl;
-
-  return 0;
+	return 0;
 }
