9b9b5426a344f079ca8b20cd5fb0632c605ecc52
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-03 00:43:07 -0400

    Update and rename Student_info.cc to Student_info.cpp

--- Accelerated C++/chapter04/Student_info.cpp
+++ Accelerated C++/chapter04/Student_info.cpp
@@ -1,7 +1,8 @@
-// source file for `Student_info'-related functions
+// source file for 'Student_info'-related functions
 #include "Student_info.h"
 
-using std::istream;  using std::vector;
+using std::istream;  
+using std::vector;
 
 bool compare(const Student_info& x, const Student_info& y) {
   return x.name < y.name;
@@ -14,15 +15,16 @@ istream& read(istream& is, Student_info& s) {
   return is;
 }
 
-// read homework grades from an input stream into a `vector<double>'
+// read homework grades from an input stream into a 'vector<double>'
 istream& read_hw(istream& in, vector<double>& hw) {
+  
   if (in) {
     // get rid of previous contents
     hw.clear();
 
     // read homework grades
-    double x;
-    while (in >> x)
+   double x;
+   while (in >> x)
       hw.push_back(x);
 
     // clear the stream so that input will work for the next student
