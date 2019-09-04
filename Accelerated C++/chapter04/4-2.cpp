d2dd43198b1993499eb602cf2438274854b66380
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-04 01:35:51 -0400

    Update 4-2.cpp

--- Accelerated C++/chapter04/4-2.cpp
+++ Accelerated C++/chapter04/4-2.cpp
@@ -1,17 +1,35 @@
+//#include<string>
+//#include<iostream>
+//#include<iomanip>
+//
+//int square(int i) {
+//	return i * i;
+//}
+//
+//int main()
+//{
+//	std::cout << "Here is the output." << std::endl;
+//	for (int i = 0; i < 10; ++i) {
+//		std::cout << std::setw(3) << i << std::setw(5) << square(i) << std::endl;
+//	}
+//	std::cout << "End of output." << std::endl;
+//    return 0;
+//}
+
 #include <cmath>
 #include <iomanip>
 #include <iostream>
 
 using namespace std;
 
-int main() {  
-  for (int i = 1; i <= 100; ++i) {
-    cout << setw(3)
-	 << i
-	 << setw(6)
-	 << i * i
-	 << endl;
-  }
-  
-  return 0;
+int main() {
+	for (int i = 1; i <= 100; ++i) {
+		cout << setw(3)
+			<< i
+			<< setw(6)
+			<< i * i
+			<< endl;
+	}
+
+	return 0;
 }
