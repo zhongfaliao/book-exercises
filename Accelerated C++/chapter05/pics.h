5371aa8efbd9acb70de116a52eceb7701601e86a
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-04 21:24:40 -0400

    Update pics.h

--- Accelerated C++/chapter05/pics.h
+++ Accelerated C++/chapter05/pics.h
@@ -3,15 +3,15 @@
 
 #include <string>
 #include <vector>
-
+ 
 std::string::size_type width(const std::vector<std::string>& v);
 
 std::vector<std::string> frame(const std::vector<std::string>& v);
-
+ 
 std::vector<std::string> vcat(const std::vector<std::string>& top,
                               const std::vector<std::string>& bottom);
-
+ 
 std::vector<std::string> hcat(const std::vector<std::string>& left,
                               const std::vector<std::string>& right);
-
+ 
 #endif
