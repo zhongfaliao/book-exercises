14694771f82d3e8747d5285d96681da31db2658f
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-05 22:12:46 -0400

    Update grade.h

--- Accelerated C++/chapter05/grade.h
+++ Accelerated C++/chapter05/grade.h
@@ -3,11 +3,11 @@
 
 #include <vector>
 #include "Student_info.h"
-
+  
 double grade(double, double, double);
 double grade(double, double, const std::vector<double>&);
 double grade(const Student_info&);
-
+  
 bool fgrade(const Student_info&);
 
 #endif
