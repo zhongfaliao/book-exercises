9c9b441d059dfc252cad5d8d61601fb36a4843bd
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-05 23:26:35 -0400

    Update median.cpp

--- Accelerated C++/chapter05/median.cpp
+++ Accelerated C++/chapter05/median.cpp
@@ -1,21 +1,20 @@
-// source file for the `median' function
+// source file for the 'median' function
 #include <algorithm>    // to get the declaration of `sort'
 #include <stdexcept>    // to get the declaration of `domain_error'
 #include <vector>       // to get the declaration of `vector'
 
-using std::domain_error;   using std::sort;   using std::vector;
+using std::domain_error;   
+using std::sort;   
+using std::vector;
 
 #include "median.h"
 
-// compute the median of a `vector<double>'
-// note that calling this function copies the entire argument `vector'
+// compute the median of a 'vector<double>'
+// note that calling this function copies the entire argument 'vector'
 double median(vector<double> vec)
 {
-#ifdef _MSC_VER
-  typedef std::vector<double>::size_type vec_sz;
-#else
+
   typedef vector<double>::size_type vec_sz;
-#endif
 
   vec_sz size = vec.size();
   if (size == 0)
