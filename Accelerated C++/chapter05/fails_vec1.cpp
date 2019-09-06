feebcbca17b221b2d103ad08e953d622349a99d1
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-05 23:31:21 -0400

    Update fails_vec1.cpp

--- Accelerated C++/chapter05/fails_vec1.cpp
+++ Accelerated C++/chapter05/fails_vec1.cpp
@@ -9,12 +9,12 @@ vector<Student_info> extract_fails(vector<Student_info>& students) {
   vector<Student_info> pass, fail;
 
    for (vector<Student_info>::size_type i = 0;
-	    i != students.size(); ++i;
+	    i != students.size(); ++i){
 	if (fgrade(students[i]))
 	   fail.push_back(students[i]);
 	else
 	   pass.push_back(students[i]);
-
+   }
    students = pass;
 
    return fail;
