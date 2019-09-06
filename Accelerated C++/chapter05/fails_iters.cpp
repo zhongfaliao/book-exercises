f5b9c3b23d3e746a5b362872daf849535628c93e
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-05 23:17:44 -0400

    Update fails_iters.cpp

--- Accelerated C++/chapter05/fails_iters.cpp
+++ Accelerated C++/chapter05/fails_iters.cpp
@@ -9,7 +9,7 @@ vector<Student_info> extract_fails(vector<Student_info>& students) {
   vector<Student_info> fail;
   
   vector<Student_info>::iterator iter = students.begin();
-
+     
   while (iter != students.end()) {
     if (fgrade(*iter)) {
       fail.push_back(*iter);
