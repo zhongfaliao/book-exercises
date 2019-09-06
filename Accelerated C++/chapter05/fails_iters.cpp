ce605d36844707b0cb9fd36bd483ee3faab1f6be
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-05 23:08:12 -0400

    Update and rename fails_iters.cc to fails_iters.cpp

--- Accelerated C++/chapter05/fails_iters.cpp
+++ Accelerated C++/chapter05/fails_iters.cpp
@@ -7,17 +7,16 @@ using std::vector;
 // version 3: iterators but no indexing; still potentially slow
 vector<Student_info> extract_fails(vector<Student_info>& students) {
   vector<Student_info> fail;
-#ifdef _MSC_VER
-  std::vector<Student_info>::iterator iter = students.begin();
-#else
+  
   vector<Student_info>::iterator iter = students.begin();
-#endif
 
   while (iter != students.end()) {
     if (fgrade(*iter)) {
       fail.push_back(*iter);
-      iter = students.erase(iter);
+      iter = students.erase(iter); // NOTE, no need to de-reference in this line!!
+      
     } else
+      
       ++iter;
   }
 
