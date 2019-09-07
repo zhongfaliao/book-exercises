d6c2285383944fe1acdeb037929b608c5d1faed0
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-07 12:32:54 -0400

    Update Student_info.cpp

--- Accelerated C++/chapter04/Student_info.cpp
+++ Accelerated C++/chapter04/Student_info.cpp
@@ -8,6 +8,7 @@ bool compare(const Student_info& x, const Student_info& y) {
   return x.name < y.name;
 }
 
+
 istream& read(istream& is, Student_info& s) {
   // read and store the student's name and midterm and final exam grades
   is >> s.name >> s.midterm >> s.final;
