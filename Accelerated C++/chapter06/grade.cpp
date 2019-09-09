7bf6ffad70f232e8c928695599a6441abfae048d
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-09 19:43:28 -0400

    Update grade.cpp

--- Accelerated C++/chapter06/grade.cpp
+++ Accelerated C++/chapter06/grade.cpp
@@ -11,6 +11,7 @@ double grade(double midterm, double final, double homework) {
   return 0.2 * midterm + 0.4 * final + 0.4 * homework;
 }
 
+
 // compute a student's overall grade from midterm and final exam grades
 // and vector of homework grades.
 // this function does not copy its argument, because `median' does so for us.
@@ -24,6 +25,7 @@ double grade(double midterm, double final, const vector<double>& hw) {
 double grade(const Student_info& s) {
   return grade(s.midterm, s.final, s.homework);
 }
+
 
 // predicate to determine whether a student failed
 bool fgrade(const Student_info& s) {
