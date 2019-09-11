8b7d62e499498fa78ceb76bf33f7a09dfe6f4843
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-10 20:47:45 -0400

    Update and rename grade_check.cc to grade_check.cpp

--- Accelerated C++/chapter06/grade_check.cpp
+++ Accelerated C++/chapter06/grade_check.cpp
@@ -4,7 +4,7 @@
 
 using std::find;
 
+
 bool did_all_hw(const Student_info& s) {
-  return ((find(s.homework.begin(), s.homework.end(), 0))
-	  == s.homework.end());
+  return ((find(s.homework.begin(), s.homework.end(), 0)) == s.homework.end());
 }
