f07be2f1e315c85156a7fd9f93cb3a0365ed3058
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-10 22:18:40 -0400

    Update ext_fails1.cc

--- Accelerated C++/chapter06/ext_fails1.cc
+++ Accelerated C++/chapter06/ext_fails1.cc
@@ -12,10 +12,8 @@ using std::vector;
 
 vector<Student_info> extract_fails(vector<Student_info>& students) {
   vector<Student_info> fail;
-  remove_copy_if(students.begin(), students.end(),
-		 back_inserter(fail), pgrade);
-  students.erase(remove_if(students.begin(), students.end(),
-			   fgrade), students.end());
+  remove_copy_if(  students.begin(), students.end(), back_inserter(fail), pgrade );
+  students.erase(  remove_if( students.begin(), students.end(), fgrade ),   students.end()  );
 
   return fail;
 }
