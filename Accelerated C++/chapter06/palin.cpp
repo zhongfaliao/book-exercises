bc1b01d8da40ffa9ab568594e9793670b5c67782
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-10 20:56:38 -0400

    Update and rename palin.cc to palin.cpp

--- Accelerated C++/chapter06/palin.cpp
+++ Accelerated C++/chapter06/palin.cpp
@@ -6,10 +6,7 @@
 using std::cin;             using std::cout;
 using std::endl;            using std::equal;
 using std::string;          using std::transform;
-
-#ifndef _MSC_VER
 using std::isspace;
-#endif
 
 bool is_palindrome(const string& s) {
   return equal(s.begin(), s.end(), s.rbegin());
