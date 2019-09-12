2a89a3a701798e0f99bf26cafb3622c545f6798e
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-11 22:20:20 -0400

    Update 6-3.cpp

--- Accelerated C++/chapter06/6-3.cpp
+++ Accelerated C++/chapter06/6-3.cpp
@@ -4,9 +4,13 @@
 using namespace std;
 
 int main() {
+  
   vector<int> u(10, 100);
   vector<int> v;
+  
+  // copy u's elements to v
+  // in the end, in a reversed order
   copy(u.begin(), u.end(), v.begin());
-
+  
   return 0;
 }
