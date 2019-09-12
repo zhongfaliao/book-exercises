ede8ea2bc1ffa2e3061193573f2504c4f765426e
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-11 22:27:01 -0400

    Update 6-3.cpp

--- Accelerated C++/chapter06/6-3.cpp
+++ Accelerated C++/chapter06/6-3.cpp
@@ -1,16 +1,28 @@
+#include "stdafx.h"
+
+#include <iostream>
+#include <iterator>
 #include <algorithm>
 #include <vector>
 
 using namespace std;
 
 int main() {
-  
-  vector<int> u(10, 100);
-  vector<int> v;
-  
-  // copy u's elements to v
-  // in the end, in a reversed order
-  copy(u.begin(), u.end(), v.begin());
-  
-  return 0;
+
+	vector<int> u = {1,2,3,4,5};
+	vector<int> v;
+
+	// copy u's elements to v
+	
+	// ERROR! the following does not compile
+	//copy(u.begin(), u.end(), v.begin());
+	
+	copy(u.begin(), u.end(), back_inserter(v));
+
+	for (vector<int>::const_iterator i = v.begin(); i != v.end(); i++)
+	{
+		cout << *i << endl;
+	}
+
+	return 0;
 }
