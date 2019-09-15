418561416436560642278b544579aaf080245e85
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-15 13:53:08 -0400

    Update grade_check.cpp

--- Accelerated C++/chapter08/grade_check.cpp
+++ Accelerated C++/chapter08/grade_check.cpp
@@ -5,6 +5,7 @@
 using std::find;
 
 bool did_all_hw(const Student_info& s) {
-  return ((find(s.homework.begin(), s.homework.end(), 0))
-	  == s.homework.end());
+  return (	(	find(s.homework.begin(), s.homework.end(), 0)   ) == s.homework.end()	);
+	// find the first location where the element of s.homework is equal to 0
+	// if no 0 is found between [begin() , end() ), then means the student did all the homework
 }
