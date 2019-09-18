21b39d3c33317881e69b6698a692a387ca82eb3f
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-17 23:11:01 -0400

    Update 8-4.hpp

--- Accelerated C++/chapter08/8-4.hpp
+++ Accelerated C++/chapter08/8-4.hpp
@@ -1,6 +1,15 @@
 
 #include <iterator>
 
+// the following do the same thing as
+/*
+#include <algorithm>
+template <class Type>
+void my_swap(Type& a, Type& b) {
+    std::swap(a, b);
+}
+*/
+
 template <class Type>
 void my_swap(Type& a, Type& b) {
   Type c = a;
