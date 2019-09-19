0ac11a8c508d12073c8289a1b1d12871994faa04
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-18 21:34:12 -0400

    Update Student_info.h

--- Accelerated C++/chapter09/Student_info.h
+++ Accelerated C++/chapter09/Student_info.h
@@ -6,12 +6,12 @@
 
 class Student_info {
  public:
-  Student_info();              // construct an empty `Student_info' object
+  Student_info();              // construct an empty 'Student_info' object
   Student_info(std::istream&); // construct one by reading a stream
   std::string name() const { return n; }
   bool valid() const { return !homework.empty(); }
 
-  // as defined in 9.2.1/157, and changed to read into `n' instead of `name'
+  // as defined in 9.2.1/157, and changed to read into 'n' instead of 'name'
   std::istream& read(std::istream&);
 
   double grade() const;    // as defined in 9.2.1/158
