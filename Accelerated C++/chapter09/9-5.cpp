25a6a8fddac6746077c1ba1dd4bfe1c12661544b
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-18 22:52:22 -0400

    Update 9-5.cpp

--- Accelerated C++/chapter09/9-5.cpp
+++ Accelerated C++/chapter09/9-5.cpp
@@ -1,20 +1,23 @@
 #include <algorithm>
 #include <iostream>
 
-#include "PF-grader.hpp"
+#include "PF-grader.h"
 
 using namespace std;
+
+// the following definitions act as the SOURCE CODE for "PF-grader.h"
 
 PF_grader::PF_grader() { }
 
 void PF_grader::add_student(Student_info student) {
   students.push_back(student);
-  sort(students.begin(), students.end(), compare);
+  
+  sort(students.begin(), students.end(), compare); // every time a new student is added
+  // sort the new container
 }
 
 void PF_grader::print_grades() const {
-  for (vector<Student_info>::const_iterator i = students.begin();
-       i != students.end(); ++i) {
+  for (vector<Student_info>::const_iterator i = students.begin(); i != students.end(); ++i) {
     string pf = (i->passing()) ? "P" : "F";
     cout << i->name() << ": " << pf << endl;
   }
