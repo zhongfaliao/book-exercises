fa395d1adbb40c5e322e06f09493f5fddd9eb7b7
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-18 22:31:26 -0400

    Update 9-1-Student_info.cpp

--- Accelerated C++/chapter09/9-1-Student_info.cpp
+++ Accelerated C++/chapter09/9-1-Student_info.cpp
@@ -15,11 +15,13 @@ bool compare(const Student_info& x, const Student_info& y) {
   return x.name() < y.name();
 }
 
-Student_info::Student_info(): midterm(0), final(0) { }
+Student_info::Student_info(): midterm(0), final(0) { } 
+// name and homework variables can automatically implicitly initialized to empty string
+// and 0.0 vector respectively, therefore no need to EXPLICITLY initialize them
 
 Student_info::Student_info(istream& is) { read(is); }	
 
-// read homework grades from an input stream into a `vector<double>'
+// read homework grades from an input stream into a 'vector<double>'
 istream& read_hw(istream& in, vector<double>& hw) {
   if (in) {
     // get rid of previous contents
@@ -40,6 +42,11 @@ istream& read_hw(istream& in, vector<double>& hw) {
 istream& Student_info::read(istream& in) {
   in >> n >> midterm >> final;
   read_hw(in, homework);
-  g = ::grade(midterm, final, homework);
+  g = ::grade(midterm, final, homework); // agian, the :: is to tell the compiler that
+  // not to call the member function, call the grade() defined somewhere else
   return in;
 }
+
+
+// note, if the default constructor is called to initialize a Student_info subject
+// the grade variable 'g' is not set, it could be anything like garbage
