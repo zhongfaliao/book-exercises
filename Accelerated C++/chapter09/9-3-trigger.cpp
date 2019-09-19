c7dd111053ec7d3d5f50e2d8fe651a9e8022f586
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-18 22:37:06 -0400

    Update 9-3-trigger.cpp

--- Accelerated C++/chapter09/9-3-trigger.cpp
+++ Accelerated C++/chapter09/9-3-trigger.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-  Student_info record;
-  record.grade();
+  Student_info record; // created but the record.homework is not defined
+  record.grade();      // trigger
   return 0;
 }
