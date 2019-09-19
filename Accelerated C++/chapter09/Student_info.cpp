482101847602c9c1f95a5f4b89e3988db231ff4a
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-18 21:39:53 -0400

    Update Student_info.cpp

--- Accelerated C++/chapter09/Student_info.cpp
+++ Accelerated C++/chapter09/Student_info.cpp
@@ -8,18 +8,22 @@ using std::istream;
 using std::vector;
 
 double Student_info::grade() const {
-  return ::grade(midterm, final, homework);
+  return ::grade(midterm, final, homework); // :: is to say grade() function in this line is not 
+  // a member function of Student_info, rather is somewhere else defined
+  // otherwise without ::, compiler would think we are calling a member function of Student_info
+  // and complain about incorrect argument list
 }
 
 bool compare(const Student_info& x, const Student_info& y) {
   return x.name() < y.name();
 }
 
-Student_info::Student_info(): midterm(0), final(0) { }
+Student_info::Student_info(): midterm(0), final(0) { } // we use "constructor initializers" to explicitly set them to zero
+// otherwise, they could be random garbage, which we should avoid
 
 Student_info::Student_info(istream& is) { read(is); }	
 
-// read homework grades from an input stream into a `vector<double>'
+// read homework grades from an input stream into a 'vector<double>'
 istream& read_hw(istream& in, vector<double>& hw) {
   if (in) {
     // get rid of previous contents
