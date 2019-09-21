5634b881a292c92b4957c30373445032430ced9c
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-21 13:18:07 -0400

    Update grade.cpp

--- Accelerated C++/chapter10/grade.cpp
+++ Accelerated C++/chapter10/grade.cpp
@@ -14,7 +14,8 @@ double grade(double midterm, double final, double homework) {
 
 // compute a student's overall grade from midterm and final exam grades
 // and vector of homework grades.
-// this function does not copy its argument, because `median' does so for us.
+// this function does not copy its argument, because 'median' does so for us.
+
 double grade(double midterm, double final, const vector<double>& hw) {
   if (hw.size() == 0)
     throw domain_error("student has done no homework");
