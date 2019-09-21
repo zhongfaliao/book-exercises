8ad23130e3b1e7e6bca7c00ba553e47db25c63bd
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-21 13:10:11 -0400

    Update Chapter-10.1.cpp

--- Accelerated C++/chapter10/Chapter-10.1.cpp
+++ Accelerated C++/chapter10/Chapter-10.1.cpp
@@ -7,11 +7,11 @@ int main()
 {
 	int x = 5;
 
-	// `p' points to `x'
+	// 'p' points to 'x'
 	int* p = &x;
 	cout << "x = " << x << endl;
 
-	// change the value of `x' through `p'
+	// change the value of 'x' through 'p'
 	*p = 6;
 	cout << "x = " << x << endl;
 	return 0;
