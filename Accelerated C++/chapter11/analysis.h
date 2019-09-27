18a16ea8a7a4ca883b1948fcb34c9a5ad09d93ab
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-09-26 22:40:30 -0400

    Update analysis.h

--- Accelerated C++/chapter11/analysis.h
+++ Accelerated C++/chapter11/analysis.h
@@ -6,9 +6,13 @@
 #include "Student_info.h"
 
 bool did_all_hw(const Student_info& s);
+
 double average_analysis(const Vec<Student_info>& students);
+
 double median_analysis(const Vec<Student_info>& students);
+
 double optimistic_median_analysis(const Vec<Student_info>& students);
+
 void write_analysis(std::ostream& out, const std::string& name,
                     double analysis(const Vec<Student_info>&),
                     const Vec<Student_info>& did,
