0122533c1edc5ec9255de12185f8e2c05003d1c7
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-10-06 18:09:58 -0400

    Update main_core_ptrs.cc

--- Accelerated C++/chapter13/main_core_ptrs.cc
+++ Accelerated C++/chapter13/main_core_ptrs.cc
@@ -1,11 +1,14 @@
+// Chapter-13.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
 #include <vector>
 #include <string>
 #include <algorithm>
 #include <iomanip>
 
-#ifndef __GNUC__
-#include <ios>
-#endif
 
 #include <iostream>
 #include <stdexcept>
@@ -23,11 +26,7 @@ using std::sort;
 using std::string;
 using std::vector;
 
-#ifdef _MSC_VER
-#include "../minmax.h"
-#else
 using std::max;
-#endif
 
 // this code almost works; see 13.3.2/242
 int main()
@@ -40,38 +39,35 @@ int main()
 	// read and store the data
 	while (cin >> ch) {
 		if (ch == 'U')
-			record = new Core;      // allocate a `Core' object
+			record = new Core;      // allocate a 'Core' object
 		else
-			record = new Grad;      // allocate a `Grad' object
-		record->read(cin);          // `virtual' call
+			record = new Grad;      // allocate a 'Grad' object
+		record->read(cin);          // 'virtual' call
 		maxlen = max(maxlen, record->name().size());// dereference
 		students.push_back(record);
 	}
 
-	// pass the version of `compare' that works on pointers
+	// pass the version of 'compare' that works on pointers
 	sort(students.begin(), students.end(), compare_Core_ptrs);
 
 	// write the names and grades
-#ifdef _MSC_VER
-	for (std::vector<Core*>::size_type i = 0;
-#else
 	for (vector<Core*>::size_type i = 0;
-#endif
-	     i != students.size(); ++i) {
-		// `students[i]' is a pointer that we dereference to call the functions
+		i != students.size(); ++i) {
+		// 'students[i]' is a pointer that we dereference to call the functions
 		cout << students[i]->name()
-		     << string(maxlen + 1 - students[i]->name().size(), ' ');
-		try {
+			<< string(maxlen + 1 - students[i]->name().size(), ' ');
+			try {
 			double final_grade = students[i]->grade();
-			streamsize prec = cout.precision();
-			cout << setprecision(3) << final_grade
-			     << setprecision(prec) << endl;
+				streamsize prec = cout.precision();
+				cout << setprecision(3) << final_grade
+				<< setprecision(prec) << endl;
 
-		} catch (domain_error e) {
+		}
+		catch (domain_error e) {
 			cout << e.what() << endl;
 		}
 		delete students[i];        // free the object allocated when reading
 	}
-	return 0;
+return 0;
 }
 
