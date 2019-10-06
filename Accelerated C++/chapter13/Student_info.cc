488da0afa90f7b3acabcd1530131dc47b9699ec8
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-10-06 16:26:51 -0400

    Update Student_info.cc

--- Accelerated C++/chapter13/Student_info.cc
+++ Accelerated C++/chapter13/Student_info.cc
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include <iostream>
 #include <stdexcept>
 
@@ -7,31 +8,30 @@
 using namespace std;
 
 istream& Student_info::read(istream& is) {
-  delete cp;
+	delete cp;
 
-  char ch;
-  is >> ch;
+	char ch;
+	is >> ch;
 
-  if (ch == 'U') cp = new Core(is);
-  else if (ch == 'G') cp = new Grad(is);
-  else if (ch == 'P') cp = new PassFail(is);
-  else if (ch == 'A') cp = new Audit(is);
-  else throw runtime_error("read invalid student type");
+	if (ch == 'U') cp = new Core(is);
+	else if (ch == 'G') cp = new Grad(is);
+	else if (ch == 'P') cp = new PassFail(is);
+	else if (ch == 'A') cp = new Audit(is);
+	else throw runtime_error("read invalid student type");
 
-  return is;
+	return is;
 }
 
-Student_info::Student_info(const Student_info& s): cp(0) {
-  if (s.cp) cp = s.cp->clone();
+Student_info::Student_info(const Student_info& s) : cp(0) {
+	if (s.cp) cp = s.cp->clone();
 }
 
 Student_info& Student_info::operator=(const Student_info& s) {
-  if (&s != this) {
-    delete cp;
-    if (s.cp) cp = s.cp->clone();
-    else cp = 0;
-  }
+	if (&s != this) {
+		delete cp;
+		if (s.cp) cp = s.cp->clone();
+		else cp = 0;
+	}
 
-  return *this;
+	return *this;
 }
-
