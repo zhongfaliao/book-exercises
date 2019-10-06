58f739c184dd77b1545dd78bc894557c3503ecff
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-10-06 16:05:36 -0400

    Update median.h

--- Accelerated C++/chapter13/median.h
+++ Accelerated C++/chapter13/median.h
@@ -9,17 +9,17 @@ using std::sort;
 
 template <class T> // this is a templated function
 T median(std::vector<T> v) {
-  
-  typedef typename std::vector<T>::size_type vec_sz;
-  
-  vec_sz size = v.size();
-  
-  if (size == 0) 
-    throw domain_error("median of an empty vector");
-  
-  sort(v.begin(), v.end());
-  
-  vec_sz mid = size / 2;
-  
-  return size % 2 == 0 ? (v[mid] + v[mid - 1]) / 2 : v[mid];
+
+	typedef typename std::vector<T>::size_type vec_sz;
+
+	vec_sz size = v.size();
+
+	if (size == 0)
+		throw domain_error("median of an empty vector");
+
+	sort(v.begin(), v.end());
+
+	vec_sz mid = size / 2;
+
+	return size % 2 == 0 ? (v[mid] + v[mid - 1]) / 2 : v[mid];
 }
