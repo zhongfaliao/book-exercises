86fb7c8dc6452d12c7a52cf556706f15a088a068
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-10-06 16:49:18 -0400

    Create read_hw.h

---
+++ Accelerated C++/chapter13/read_hw.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+
+// read homework grades from an input stream into a 'vector<double>'
+std::istream& read_hw(std::istream& in, vector<double>& hw)
