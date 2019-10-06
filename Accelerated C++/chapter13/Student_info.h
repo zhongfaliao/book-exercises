e22cb36a5bba043ca0558c7a8ec69d2df0b6dfe0
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-10-06 16:25:43 -0400

    Update Student_info.h

--- Accelerated C++/chapter13/Student_info.h
+++ Accelerated C++/chapter13/Student_info.h
@@ -11,8 +11,8 @@
 class Student_info {
 public:
 	// constructors and copy control
-	Student_info(): cp(0) { }
-	Student_info(std::istream& is): cp(0) { read(is); }
+	Student_info() : cp(0) { }
+	Student_info(std::istream& is) : cp(0) { read(is); }
 	Student_info(const Student_info&);
 	Student_info& operator=(const Student_info&);
 	~Student_info() { delete cp; }
@@ -30,13 +30,12 @@ public:
 	}
 
 	static bool compare(const Student_info& s1,
-	                    const Student_info& s2) {
+		const Student_info& s2) {
 		return s1.name() < s2.name();
 	}
 
 private:
-	Core* cp;
+	Core * cp;
 };
 
 #endif
-
