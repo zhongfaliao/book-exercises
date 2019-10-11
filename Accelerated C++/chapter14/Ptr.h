dedb1a795a281bfc5f4a461e96614e4d1ea8c3b3
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-10-10 22:33:05 -0400

    Update Ptr.h

--- Accelerated C++/chapter14/Ptr.h
+++ Accelerated C++/chapter14/Ptr.h
@@ -6,60 +6,61 @@
 #include "Ref_count.hpp"
 
 template <class T> class Ptr {
- public:
-  void make_unique() {
-    if (refptr.val() != 1) {
-      --refptr;
-      refptr = Ref_count();
-      p = p? clone(p): 0;
-    }
-  }
+public:
+	void make_unique() {
+		if (refptr.val() != 1) {
+			--refptr;
+			refptr = Ref_count();
+			p = p ? clone(p) : 0;
+		}
+	}
 
- Ptr(): p(0) { }
- Ptr(T* t): p(t) { }
- Ptr(const Ptr& h): p(h.p), refptr(h.refptr) { ++refptr; }
+	Ptr() : p(0) { }
+	Ptr(T* t) : p(t) { }
+	Ptr(const Ptr& h) : p(h.p), refptr(h.refptr) { ++refptr; }
 
-  Ptr& operator=(const Ptr&);
-  ~Ptr();
-  operator bool() const { return p; }
-  T& operator*() const;
-  T* operator->() const;
+	Ptr& operator=(const Ptr&);
+	~Ptr();
+	operator bool() const { return p; }
+	T& operator*() const;
+	T* operator->() const;
 
- private:
-  T* p;
-  Ref_count refptr;
+private:
+	T * p;
+	Ref_count refptr;
 };
 
 template <class T> T* clone(const T* tp) {
-  return tp->clone();
+	return tp->clone();
 }
 
-template<class T> T& Ptr<T>::operator*() const { 
-  if (p) return *p; throw std::runtime_error("unbound Ptr"); 
+template<class T> T& Ptr<T>::operator*() const {
+	if (p) return *p; throw std::runtime_error("unbound Ptr");
 }
 
-template<class T> T* Ptr<T>::operator->() const { 
-  if (p) return p; throw std::runtime_error("unbound Ptr"); 
+template<class T> T* Ptr<T>::operator->() const {
+	if (p) return p; throw std::runtime_error("unbound Ptr");
 }
 
 template<class T> Ptr<T>& Ptr<T>::operator=(const Ptr& rhs) {
-  ++rhs.refptr;
-  --refptr;
+	++rhs.refptr;
+	--refptr;
 
-  if (refptr.val() == 0) {
-    delete p;
-  }
+	if (refptr.val() == 0) {
+		delete p;
+	}
 
-  refptr = rhs.refptr;
-  p = rhs.p;
+	refptr = rhs.refptr;
+	p = rhs.p;
 
-  return *this;
+	return *this;
 }
 
-  template<class T> Ptr<T>::~Ptr() {
-    --refptr;
+template<class T> Ptr<T>::~Ptr() {
+	--refptr;
 
-    if (refptr.val() == 0) {
-      delete p;
-    }
-  }
+	if (refptr.val() == 0) {
+		delete p;
+	}
+}
+
