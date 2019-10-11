295eec007c5b9c4ec26207e6dcecccbaa96a9b88
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-10-10 22:25:55 -0400

    Update Handle.h

--- Accelerated C++/chapter14/Handle.h
+++ Accelerated C++/chapter14/Handle.h
@@ -3,19 +3,19 @@
 
 template <class T> class Handle {
 public:
-	Handle(): p(0) { }
-	Handle(const Handle& s): p(0) { if (s.p) p = s.p->clone(); }
+	Handle() : p(0) { }
+	Handle(const Handle& s) : p(0) { if (s.p) p = s.p->clone(); }
 	Handle& operator=(const Handle&);
 	~Handle() { delete p; }
 
-	Handle(T* t): p(t) { }
+	Handle(T* t) : p(t) { }
 
 	operator bool() const { return p; }
 	T& operator*() const;
 	T* operator->() const;
 
 private:
-	T* p;
+	T * p;
 };
 
 #include <stdexcept>
@@ -35,7 +35,7 @@ Handle<T>& Handle<T>::operator=(const Handle& rhs)
 template <class T>
 T& Handle<T>::operator*() const
 {
-	if (p) 	
+	if (p)
 		return *p;
 	throw runtime_error("unbound Handle");
 }
