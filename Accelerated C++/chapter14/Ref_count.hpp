8d352d869a35a9cd9ab4e2f5bd1f0cfee4c61104
zhongfaliao <38413816+zhongfaliao@users.noreply.github.com>
2019-10-10 22:34:39 -0400

    Update Ref_count.hpp

--- Accelerated C++/chapter14/Ref_count.hpp
+++ Accelerated C++/chapter14/Ref_count.hpp
@@ -4,12 +4,13 @@
 
 class Ref_count {
 public:
-  Ref_count(): count(new std::size_t(1)) { }
+	Ref_count() : count(new std::size_t(1)) { }
 
-  std::size_t val() const { return *count; }
-  void operator ++() const { ++*count; }
-  void operator --() const { if (--*count == 0) delete count; }
+	std::size_t val() const { return *count; }
+	void operator ++() const { ++*count; }
+	void operator --() const { if (--*count == 0) delete count; }
 
 private:
-  std::size_t* count;
+	std::size_t* count;
 };
+
